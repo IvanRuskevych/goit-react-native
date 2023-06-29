a57ee1324405e843c8957357386be808895ae0c1
unknown <ivan.ruskevych@gmail.com>
2023-06-29 08:01:56 +0300

    hw-04 textbook NOT FOR MERGE

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from './src/Screens/Home/HomeScreen';
 import { LoginScreen } from './src/Screens/LoginScreen/LoginScreen';
 import { RegistrationScreen } from './src/Screens/RegistrationScreen/RegistrationScreen';
+import { Button } from 'react-native';
 
 export default function App() {
   const MainStack = createStackNavigator();
@@ -19,7 +20,22 @@ export default function App() {
         <MainStack.Screen
           name="HomeScreen"
           component={HomeScreen}
-          options={{ title: 'Start screen' }}
+          options={{
+            title: 'Start screen',
+            headerStyle: { backgroundColor: '#f4511e' },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 25,
+            },
+            headerRight: () => (
+              <Button
+                title="Press me"
+                color="blue"
+                onPress={() => alert('This is a button')}
+              />
+            ),
+          }}
         />
         {/* <RegistrationScreen /> */}
         {/* <LoginScreen /> */}
